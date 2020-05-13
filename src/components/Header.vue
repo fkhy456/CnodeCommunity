5cb5975b6a6d8822415673d419bbd8df4d6b7abc
Gjw <1160832331@qq.com>
2020-05-13 17:23:45 +0800

    提交文件

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="header"> 
+        <router-link :to="{name:'root'}">
+            <img src="../assets/cnodejs_light.svg">
+        </router-link>
+        <ul>
+            <router-link :to="{name:'root'}"><li><a href="#">首页</a></li></router-link>
+            <li><a href="#">新手入门</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">关于</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">登录</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.header{
+    background-color:#444;
+    height: 50px;
+    margin: -8px -8px 0px -8px;
+}
+img{
+    max-width:120px;
+    margin-left:60px;
+    margin-top:10px;
+    padding-left: 100px;
+}
+ul{
+    list-style:none;
+    float: right;
+    margin: 7px 0;
+    padding-right: 250px;
+}
+li{
+    display: inline-block;
+    padding: 0 10px;
+}
+a{
+    text-decoration: none;
+    color: #ccc;
+    font-size: 14px;
+    text-shadow: none;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Article from '../components/Article'
+import PostList from '../components/PostList'
+import UserInfo from '../components/UserInfo'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      name:'root',
+      path:'/',
+      components:{
+        main:PostList
+      }
+    },
+    {
+      name:'post_content',
+      path:'/topic/:id',
+      components:{
+        main:Article
+      }
+    },
+    {
+      name:'user_info',
+      path:'/userinfo/:name',
+      components:{
+        main:UserInfo
+      }
+    }
+  ]
+})
